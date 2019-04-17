bf58abe4e50ee48df09fb4c26135ba6e69ad42c1
Pedro Neiva Alves <neiva@Neivas-MacBook-Pro.local>
2019-04-17 15:10:32 -0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Bye Bye there
+            Estou rodando na AWS \o/
           </p>
           <a
             className="App-link"
